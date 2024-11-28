85710e7c4ea3de5cad5af7c32e075bf2180fe118
Polymathh <wambugum110@gmail.com>
2024-11-28 17:21:14 +0300

    project done

--- lib/resources/repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:weather_app/models/weather.dart';
-import 'package:weather_app/resources/weather_api.dart';
-
-class Repository {
-  static Future<WeeklyWeather> fetchWeather(String cityName) =>
-      fetchWeeklyWeather(cityName);
-}
